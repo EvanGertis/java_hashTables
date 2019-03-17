631e40666dc560a4733fd8db6d7e963eb0a53eed
EvanGertis <39931491+EvanGertis@users.noreply.github.com>
2019-03-17 15:16:18 -0400

    added employee class

--- src/com/company/Employee.java
+++ src/com/company/Employee.java
@@ -1,5 +1,57 @@
 package com.company;
+/*
+* File Name: Employee.java
+* Author: Evan Gertis
+* Date: 03/17/2019
+* Purpose: Class for storing methods and memebers for implementing and Employee hash table.
+*/
 
 public class Employee {
 
+    //private class memebers
+    private String mFirstName;
+    private String mLastName;
+    private int mId;
+    //**********************
+
+    //Constructor initializes memebers.
+    public Employee(String firstName, String lastName, int id){
+        this.mFirstName = firstName;
+        this.mLastName = lastName;
+        this.mId = id;
+    }
+    //********************************
+
+    //BEGIN CLASS METHODS
+
+    //**GETTERS**//
+    public String getFirstName() {
+        return getFirstName();
+    }
+
+    public String getLastName() {
+        return getLastName();
+    }
+
+    public String getId () {
+        return getId();
+    }
+    //**********//
+
+    //**SETTERS**//
+    public void setFirstName(String firstName){
+        this.mFirstName = firstName;
+    }
+
+    public void setLastName(String lastName){
+        this.mLastName = lastName;
+    }
+
+    public void setId(int id){
+        this.mId = id;
+    }
+    //***********//
+
+
+    //END CLASS METHODS
 }
