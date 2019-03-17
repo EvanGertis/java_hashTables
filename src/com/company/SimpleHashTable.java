d47df8640731bbe719d98df8b5f5958d16a798c6
EvanGertis <39931491+EvanGertis@users.noreply.github.com>
2019-03-17 15:28:23 -0400

    added put and get

--- src/com/company/SimpleHashTable.java
+++ src/com/company/SimpleHashTable.java
@@ -15,6 +15,23 @@ public class SimpleHashTable {
         hashtable = new Employee[10];
     }
 
+    //put method, allows user to put stuff in the table.
+    public void put(String key, Employee employee){
+        int hashedKey = hashKey(key);
+        if(hashtable[hashedKey] != null){
+            System.out.println("Sorry there is already an employee in this index");
+        } else {
+            hashtable[hashedKey] = employee;
+        }
+
+    }
+
+    //get from hashtable.
+    public Employee get(String key){
+        int hashedKey = hashKey(key);
+        return hashtable[hashedKey];
+    }
+
     //hashing function.
     private int hashKey(String key){
         return key.length() % hashtable.length;
